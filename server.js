6af446c9efee41738f4573fce5985abfda3eb5e8
Sebastian Odell <sebastian@sanctuary.computer>
2017-06-07 13:45:58 -0400

    First Pass

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const Hapi   = require('hapi');
+const Wreck  = require('wreck');
+const Parser = require('xml2json');
+const server = new Hapi.Server();
+
+const defaultFeedName = 'sanctuary-computer-inc';
+const xmlEndpoint = `https://medium.com/feed/${process.env.MEDIUM_FEED_NAME || defaultFeedName}`;
+
+server.connection({
+  port: (process.env.PORT || 4134),
+  host: '0.0.0.0',
+});
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    Wreck.get(xmlEndpoint, (err, res, payload) => {
+      if (err) return reply(`Error: ${err}`);
+      return reply(Parser.toJson(payload, { object: true, sanitize: false }));
+    });
+  }
+});
+
+server.start((err) => {
+  if (err) throw err;
+  console.log(`~~~> Server running at: ${server.info.uri}`);
+});
